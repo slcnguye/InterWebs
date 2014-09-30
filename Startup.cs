f60f137b9fcb9cec94fcbec1299c69846775734a
slcnguye <slcnguye@uwaterloo.ca>
2014-09-30 00:20:51 -0400

    First commit on interwebs project

---
+++ Startup.cs
@@ -0,0 +1,15 @@
+﻿using InterWebs;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace InterWebs
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
