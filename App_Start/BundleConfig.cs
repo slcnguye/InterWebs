c32b0019da93c4c113961c852fdf39a48cbb4341
slcnguye <slcnguye@gmail.com>
2014-11-28 16:52:43 -0500

    Changed less extension used and cleaned up bundleconfig.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -1,44 +1,47 @@
-﻿using System.Web.Optimization;
-
-namespace InterWebs
+﻿namespace InterWebs
 {
+    using System.Web.Optimization;
+
     public class BundleConfig
     {
-        // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
+        public const string BundlesSite = "~/bundles/site";
+        public const string BundlesJquery = "~/bundles/jquery";
+        public const string BundlesKnockout = "~/bundles/knockout";
+        public const string BundlesModernizr = "~/bundles/modernizr";
+        public const string BundlesBootstrap = "~/bundles/bootstrap";
+        public const string BundlesI18N = "~/bundles/i18n";
+        public const string ContentStyles = "~/Content/styles";
+
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/site")
+            bundles.Add(new ScriptBundle(BundlesSite)
                 .IncludeDirectory("~/Scripts/site/knockout-models", "*.js", true)
                 .IncludeDirectory("~/Scripts/site/knockout-extenders", "*.js", true)
                 .IncludeDirectory("~/Scripts/site/knockout-bindings", "*.js", true));
 
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js").Include(
-                        "~/Scripts/jquery.signalR-2.1.2.js"));
+            bundles.Add(new ScriptBundle(BundlesJquery)
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.signalR-2.1.2.js")
+                .Include("~/Scripts/jquery.validate*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
-            "~/Scripts/knockout-{version}.js",
-            "~/Scripts/namespace.js"));
+            bundles.Add(new ScriptBundle(BundlesKnockout)
+                .Include("~/Scripts/knockout-{version}.js")
+                .Include("~/Scripts/namespace.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            bundles.Add(new ScriptBundle(BundlesModernizr)
+                .Include("~/Scripts/modernizr-*"));
 
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            bundles.Add(new ScriptBundle(BundlesBootstrap)
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/respond.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            bundles.Add(new ScriptBundle(BundlesI18N)
+                .Include("~/Scripts/i18next-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/i18n").Include(
-                "~/Scripts/i18next-{version}.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/Styles/bootstrap.css",
-                      "~/Content/Styles/site.less",
-                      "~/Content/Styles/font-awesome.css"));
+            bundles.Add(new StyleBundle(ContentStyles)
+                .Include("~/Content/Styles/bootstrap.css")
+                .Include("~/Content/Styles/site.less")
+                .Include("~/Content/Styles/font-awesome.css"));
         }
     }
 }
